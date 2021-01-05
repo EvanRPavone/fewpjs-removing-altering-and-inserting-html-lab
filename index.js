c6b6767457db177d38fa6f25f33acc5da7abe5aa
EvanRPavone <evanrpavone@gmail.com>
2021-01-05 11:17:17 -0500

    commit tree c6b4c5f84e53e0ae09242d0bf8537951dfb09cc5

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+// document.querySelector('main#main').remove();
+
+// let newHeader = document.createElement('h1');
+
+// newHeader.setAttribute('id', 'victory')
+// newHeader.innerHTML= '<h1>victory</h1>';
+// newHeader.innerText = 'EVAN is the champion';
+
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "EVAN is the champion"
